3a4742763e7d489804b95fc3a5a6fe0e55bd6b3b
Diogomadeira97 <83920663+Diogomadeira97@users.noreply.github.com>
2025-11-21 11:36:37 -0300

    Update default-server.sh

--- Conf/default/default-server.sh
+++ Conf/default/default-server.sh
@@ -10,7 +10,7 @@ apt install python3-certbot-nginx python3-certbot-dns-cloudflare -y
 cd /mnt/Cloud/Data/Dietpi-NAS_Exential/Conf/Nginx
 
 #Create Nginx config to Domain.
-echo -e "server{\n	listen 80 default_server;\n	listen [::]:80 default_server;\n\n	listen 443 default_server;\n	listen [::]:443 default_server;\n	ssl_reject_handshake on;\n	server_name _;\n	return 444;	\n}\n\n" >> $1
+echo -e "server{\n	listen 80 default_server;\n	listen [::]:80 default_server;\n\n	listen 443 ssl default_server;\n	listen [::]:443 ssl default_server;\n	http2 on;\n	ssl_reject_handshake on;\n	server_name _;\n	return 444;	\n}\n\n" >> $1
 echo -e "server{\n	listen 80;\n	listen [::]:80;\n	server_name $1$2;\n	return 301 https://\$host\$request_uri;	\n}\n\nserver{\n	listen 443 ssl;\n	listen [::]:443 ssl;\n	http2 on;\n	server_name $1$2;\n	root /var/www/$1;\n\n	ssl_certificate /etc/letsencrypt/live/$1$2/fullchain.pem;\n	ssl_certificate_key /etc/letsencrypt/live/$1$2/privkey.pem;\n}" >> $1
 
 #Edit index.html of Domain.
